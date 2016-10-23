3a9300e212e209da8f61888e89cb12b490d3f779
Jong Hoon Lee <j94117@gmail.com>
2016-10-23 18:54:22 +0100

    Extra files added

--- Coursework/main.cpp
+++ Coursework/main.cpp
@@ -1,10 +1,20 @@
 #include "Encoder.h"
+#include <algorithm>
 
 int main() {
 	Encoder en;
 
-	en.read_file("test1.txt");
+	en.read_file("binaryFile.txt");
 	//en.write_file("test_out.txt", "0010111000010001110");
-	en.encode("0111");
+	en.encode("1010");
+
+	int myints[] = { 0,0,1,1 };
+
+	do {
+		std::cout << myints[0] << ' ' << myints[1] << ' ' << myints[2] << ' ' << myints[3] << '\n';
+	} while (std::next_permutation(myints, myints + 4));
+	
+	std::cout << "After loop: " << myints[0] << ' ' << myints[1] << ' ' << myints[2] << ' ' << myints[3] << '\n';
+
 	return 0;
 }
