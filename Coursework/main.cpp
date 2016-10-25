892ccad430f402e8744f87dc2c6eb6616219bf41
Jong Hoon Lee <j94117@gmail.com>
2016-10-25 11:28:35 +0100

    Fixed main

--- Coursework/main.cpp
+++ Coursework/main.cpp
@@ -4,7 +4,6 @@
 int main() {
 	Encoder en;
 
-<<<<<<< HEAD
 	//en.read_file("test1.txt");
 	//en.write_file("test_out.txt", "0010111000010001110");
 	en.encode("11111");
@@ -23,19 +22,6 @@ int main() {
 	do {
 		cout << my_ints[0] << my_ints[1] << my_ints[2] << my_ints[3] << endl;
 	} while (std::next_permutation(my_ints, my_ints + 4));
-=======
-	en.read_file("binaryFile.txt");
-	//en.write_file("test_out.txt", "0010111000010001110");
-	en.encode("1010");
-
-	int myints[] = { 0,0,1,1 };
-
-	do {
-		std::cout << myints[0] << ' ' << myints[1] << ' ' << myints[2] << ' ' << myints[3] << '\n';
-	} while (std::next_permutation(myints, myints + 4));
-	
-	std::cout << "After loop: " << myints[0] << ' ' << myints[1] << ' ' << myints[2] << ' ' << myints[3] << '\n';
->>>>>>> refs/remotes/origin/master
 
 	return 0;
 }
